7a2f30d27337b6e8239ba9304309694a15b80495
Adi Munawar <munawar.adi31@gmail.com>
2022-05-11 15:50:48 +0700

    remove unused import

--- movie/lib/presentation/pages/home_movie_page.dart
+++ movie/lib/presentation/pages/home_movie_page.dart
@@ -2,7 +2,6 @@ import 'package:core/core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie/movie.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 class HomeMoviePage extends StatefulWidget {
