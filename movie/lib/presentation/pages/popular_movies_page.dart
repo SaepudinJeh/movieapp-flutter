4f04b72faec8ccf0d5f8c0c5b5c6aab52bc37f20
Adi Munawar <munawar.adi31@gmail.com>
2022-05-11 07:58:08 +0700

    implment bloc in popular movies page

--- movie/lib/presentation/pages/popular_movies_page.dart
+++ movie/lib/presentation/pages/popular_movies_page.dart
@@ -1,7 +1,6 @@
-import 'package:core/core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie/movie.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class PopularMoviesPage extends StatefulWidget {
   static const routeName = '/popular_movie';
@@ -16,7 +15,7 @@ class _PopularMoviesPageState extends State<PopularMoviesPage> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(() => Provider.of<PopularMoviesNotifier>(context, listen: false).fetchPopularMovies());
+    Future.microtask(() => context.read<PopularMoviesBloc>().add(FetchPopularMovies()));
   }
 
   @override
@@ -27,24 +26,28 @@ class _PopularMoviesPageState extends State<PopularMoviesPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Consumer<PopularMoviesNotifier>(
-          builder: (context, data, child) {
-            if (data.state == RequestState.Loading) {
+        child: BlocBuilder<PopularMoviesBloc, PopularMoviesState>(
+          builder: (context, state) {
+            if (state is PopularMoviesLoading) {
               return Center(
                 child: CircularProgressIndicator(),
               );
-            } else if (data.state == RequestState.Loaded) {
+            } else if (state is PopularMoviesHasData) {
               return ListView.builder(
                 itemBuilder: (context, index) {
-                  final movie = data.movies[index];
+                  final movie = state.result[index];
                   return MovieCard(movie);
                 },
-                itemCount: data.movies.length,
+                itemCount: state.result.length,
+              );
+            } else if (state is PopularMoviesError) {
+              return Center(
+                key: Key('error_message'),
+                child: Text(state.message),
               );
             } else {
               return Center(
-                key: Key('error_message'),
-                child: Text(data.message),
+                child: Text('Failed'),
               );
             }
           },
