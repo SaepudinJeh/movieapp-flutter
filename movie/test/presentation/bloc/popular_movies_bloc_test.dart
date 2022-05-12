c6f21d86a43bcffbd7c1236f678c2111cbd1d529
Adi Munawar <munawar.adi31@gmail.com>
2022-05-12 08:13:44 +0700

    create detail movies test

---
+++ movie/test/presentation/bloc/popular_movies_bloc_test.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:movie/movie.dart';
+
+@GenerateMocks([GetPopularMovies])
+void main() {}
---
+++ movie/test/presentation/bloc/watchlist_movies_bloc_test.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:movie/movie.dart';
+
+@GenerateMocks([GetWatchlistMovies, SaveWatchlist, RemoveWatchlist, GetWatchListStatus])
+void main() {}
